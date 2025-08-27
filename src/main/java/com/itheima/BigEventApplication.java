7ce576e07bdca7984d5f6e2bd9c08f0ed585a88a
crRdz <crrrrdz@gmail.com>
2025-08-27 12:09:47 +0800

    initial commit

---
+++ src/main/java/com/itheima/BigEventApplication.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigEventApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(BigEventApplication.class, args);
+    }
+}
