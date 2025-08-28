e485c1597ed3864d8088a3441213f38808c9049e
crRdz <crrrrdz@gmail.com>
2025-08-28 23:46:25 +0800

    Configure the interceptor changes:LoginInterceptor,WebConfig,ArticleController testMethodology:Use Postman send the request to ArticleList and check the output

--- src/main/java/com/itheima/controller/ArticleController.java
+++ src/main/java/com/itheima/controller/ArticleController.java
@@ -14,15 +14,7 @@ import java.util.Map;
 @RequestMapping("/article")
 public class ArticleController {
     @GetMapping("/list")
-    public Result<String> list(@RequestHeader(name = "Authorization") String token, HttpServletResponse response) {
-        //验证token
-        try {
-            Map<String, Object> claims = JwtUtil.parseToken(token);
-            return Result.success("所有的文章列表数据...");
-        } catch (Exception e) {
-            //http响应状态码为401
-            response.setStatus(401);
-            return Result.error("用户未登录");
-        }
+    public Result<String> list() {
+        return Result.success("所有的文章数据...");
     }
 }
