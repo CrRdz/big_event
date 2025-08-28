fd8f0c4234413c0ed6bab5cdc12b219c20d78beb
crRdz <crrrrdz@gmail.com>
2025-08-28 23:36:19 +0800

    mistake changes(order)

--- src/main/java/com/itheima/controller/ArticleController.java
+++ src/main/java/com/itheima/controller/ArticleController.java
@@ -18,11 +18,11 @@ public class ArticleController {
         //验证token
         try {
             Map<String, Object> claims = JwtUtil.parseToken(token);
+            return Result.success("所有的文章列表数据...");
         } catch (Exception e) {
             //http响应状态码为401
             response.setStatus(401);
             return Result.error("用户未登录");
         }
-        return Result.success("所有的文章列表数据...");
     }
 }
