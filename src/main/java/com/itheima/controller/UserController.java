eca993a2642bbf9831969acb6a566fa6b5a1672e
crRdz <crrrrdz@gmail.com>
2025-08-28 20:45:52 +0800

    Register interface implementation testMethodology：Use postman to send a request, check for accepted data changes：UserController，UserService，UserServiceImpl，UserMapper

---
+++ src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Result;
+import com.itheima.pojo.User;
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @PostMapping("/register")
+    public Result register(String username, String password){
+
+        //1.查询用户
+        User u = userService.findByUsername(username);
+        if(u == null){
+            //2.没有用户，注册用户
+            userService.register(username,password);
+            return Result.success("注册成功");
+        }else{
+            //3.有用户，返回用户已存在
+            return Result.error("用户已被占用");
+        }
+    }
+}
---
+++ src/main/java/com/itheima/service/UserService.java
@@ -0,0 +1,12 @@
+package com.itheima.service;
+
+import com.itheima.pojo.User;
+
+public interface UserService {
+
+    //根据用户名查询用户
+    User findByUsername(String username);
+
+    //用户注册
+    void register(String username, String password);
+}
