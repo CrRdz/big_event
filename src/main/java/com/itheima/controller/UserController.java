78e1ca6291be540a53c4f70b93d1cd60edfb500b
crRdz <crrrrdz@gmail.com>
2025-08-28 21:03:36 +0800

    Parameter verification testMethodology：Use postman to send a request, check for accepted data changes：UserController

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -16,16 +16,21 @@ public class UserController {
     private UserService userService;
     @PostMapping("/register")
     public Result register(String username, String password){
-
-        //1.查询用户
-        User u = userService.findByUsername(username);
-        if(u == null){
-            //2.没有用户，注册用户
-            userService.register(username,password);
-            return Result.success("注册成功");
-        }else{
-            //3.有用户，返回用户已存在
-            return Result.error("用户已被占用");
+        if(username == null && username.length() >= 5 && username.length() <= 16 &&
+        password != null && password.length() >= 5 && password.length() <= 16
+        ) {
+            //1.查询用户
+            User u = userService.findByUsername(username);
+            if (u == null) {
+                //2.没有用户，注册用户
+                userService.register(username, password);
+                return Result.success("注册成功");
+            } else {
+                //3.有用户，返回用户已存在
+                return Result.error("用户已被占用");
+            }
+        }else {
+            return Result.error("用户名或密码不符合规范");
         }
     }
 }
