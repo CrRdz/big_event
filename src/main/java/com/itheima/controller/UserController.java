ea4d7f36de43c5f0881d860aa418ca410b821d23
crRdz <crrrrdz@gmail.com>
2025-08-28 21:28:46 +0800

    Implement the registration interface parameter verification through SpringValidation testMethodology：Use postman to send a request, check the console output changes：UserController，GlobalExceptionHandler notes：Add @Pattern annotations to parameters, specify validation rules, add @Validated annotations to the Controller class, and handle exceptions that fail parameter validation in the global exception handler 在参数上添加@Pattern注解，指定校验规则，在Controller类上添加@Validated注解，在全局异常处理器中处理参数校验失败的异常

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -3,34 +3,31 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public Result register(String username, String password){
-        if(username == null && username.length() >= 5 && username.length() <= 16 &&
-        password != null && password.length() >= 5 && password.length() <= 16
-        ) {
-            //1.查询用户
-            User u = userService.findByUsername(username);
-            if (u == null) {
-                //2.没有用户，注册用户
-                userService.register(username, password);
-                return Result.success("注册成功");
-            } else {
-                //3.有用户，返回用户已存在
-                return Result.error("用户已被占用");
-            }
-        }else {
-            return Result.error("用户名或密码不符合规范");
+    public Result register(@Pattern(regexp = "^\\S{5,16}$")String username, @Pattern(regexp = "^\\S{5,16}$")String password){
+        //1.查询用户
+        User u = userService.findByUsername(username);
+        if (u == null) {
+            //2.没有用户，注册用户
+            userService.register(username, password);
+            return Result.success("注册成功");
+        } else {
+            //3.有用户，返回用户已存在
+            return Result.error("用户已被占用");
         }
     }
 }
