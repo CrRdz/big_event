02e88d11679b122bf97ff9bc1e0130089b763b06
crRdz <crrrrdz@gmail.com>
2025-08-30 13:26:17 +0800

    Parameter validation of the New Article interface testMethodology：Postman changes：ArticleController，Article，State，StateValidation notes：Customize @state annotations to achieve custom verification 自定义@state注解，实现自定义校验

---
+++ src/main/java/com/itheima/anno/State.java
@@ -0,0 +1,26 @@
+package com.itheima.anno;
+
+import com.itheima.validation.StateValidation;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Documented//元注解
+@Constraint(validatedBy = { StateValidation.class})//指定提供校验规则的类
+@Target({FIELD})//元注解
+@Retention(RUNTIME)
+
+public @interface State {
+    //提供校验失败后的提示语
+    String message() default "{state参数的值只能是已发布或者草稿}";
+    //指定分组
+    Class<?>[] groups() default {};
+    //负载 获取到State注解的附加信息
+    Class<? extends Payload>[] payload() default {};
+
+}
