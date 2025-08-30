1eec53efba3b3d8dde3475cb3a6333fc21ee0783
crRdz <crrrrdz@gmail.com>
2025-08-30 15:19:25 +0800

    Article classification list is paginated TestMethodology：Postman changes：ArticleController，PageBean，ArticleService，ArticleServiceImpl，ArticleMapper notes： 创建PageBean对象：创建用于封装分页结果的对象开启分页查询：使用PageHelper.startPage()设置分页参数 执行查询：从ThreadLocal中获取当前用户ID 调用articleMapper.list()方法查询文章列表，传入用户ID、分类ID和文章状态作为查询条件 类型转换：将查询结果转换为Page<Article>类型，以便获取分页信息 封装结果：设置总记录数和当前页数据到PageBean对象中

--- src/main/java/com/itheima/mapper/ArticleMapper.java
+++ src/main/java/com/itheima/mapper/ArticleMapper.java
@@ -4,6 +4,8 @@ import com.itheima.pojo.Article;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ArticleMapper {
 
@@ -11,4 +13,7 @@ public interface ArticleMapper {
     @Insert("insert into article(title,content,cover_img,state,category_id,create_user,create_time,update_time) " +
             "values(#{title},#{content},#{coverImg},#{state},#{categoryId},#{createUser},now(),now())")
     void add(Article article);
+
+    //条件分页列表查询
+    List<Article> list(Integer userId, Integer categoryId, String state);
 }
