9d9bfb18584ae6211af022cc2f4800a38af1b6e0
crRdz <crrrrdz@gmail.com>
2025-09-04 18:01:15 +0800

    Delete the article classification interface testMethodology：Postman

--- src/main/java/com/itheima/mapper/CategoryMapper.java
+++ src/main/java/com/itheima/mapper/CategoryMapper.java
@@ -1,10 +1,7 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.Category;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -26,4 +23,8 @@ public interface CategoryMapper {
     //更新
     @Update("update category set category_name = #{categoryName},category_alias = #{categoryAlias},update_time = now() where id = #{id}")
     void update(Category category);
+
+    //删除
+    @Delete("delete from category where id=#{id}")
+    void deleteById(Integer id);
 }
--- src/main/java/com/itheima/service/CategoryService.java
+++ src/main/java/com/itheima/service/CategoryService.java
@@ -17,4 +17,7 @@ public interface CategoryService {
 
     //更新分类
     void update(Category category);
+
+    //删除分类
+    void deleteById(Integer id);
 }
